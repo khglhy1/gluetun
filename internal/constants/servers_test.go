53da48c46a0ab32c272a168e5b14790b6895d08e
Quentin McGaw <quentin.mcgaw@gmail.com>
2021-02-14 18:55:49 +0000

    FastestVPN servers information

--- internal/constants/servers_test.go
+++ internal/constants/servers_test.go
@@ -39,6 +39,11 @@ func Test_versions(t *testing.T) {
 			version: allServers.Cyberghost.Version,
 			digest:  "fd6242bb",
 		},
+		"Fastestvpn": {
+			model:   models.FastestvpnServer{},
+			version: allServers.Fastestvpn.Version,
+			digest:  "8825919b",
+		},
 		"Mullvad": {
 			model:   models.MullvadServer{},
 			version: allServers.Mullvad.Version,
@@ -124,6 +129,11 @@ func Test_timestamps(t *testing.T) {
 			servers:   allServers.Cyberghost.Servers,
 			timestamp: allServers.Cyberghost.Timestamp,
 			digest:    "5d3a8cbf",
+		},
+		"Fastestvpn": {
+			servers:   allServers.Fastestvpn.Version,
+			timestamp: allServers.Fastestvpn.Timestamp,
+			digest:    "da65734a",
 		},
 		"Mullvad": {
 			servers:   allServers.Mullvad.Servers,
